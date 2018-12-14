50421b9d2bf04249b83984b6d695af01c6ff9ffd
Hai Lin Zhang <hzhan339@hotmail.com>
2018-12-13 22:10:36 -0800

    http calls

--- src/app/core/services/NHL-API.service/nhl-api.service.service.ts
+++ src/app/core/services/NHL-API.service/nhl-api.service.service.ts
@@ -9,7 +9,20 @@ export class NHLService {
   constructor(private http: HttpClient) {
   }
 
-  public getCurrentStandings(): Promise<any> {
-      return this.http.get('https://statsapi.web.nhl.com/api/v1/standings').toPromise();
+  public async getCurrentStandings(): Promise<any> {
+      return await this.http.get('https://statsapi.web.nhl.com/api/v1/standings');
+  }
+
+    /**
+     * Gets schedule for current day
+     */
+  public async getTodaysSchedule(): Promise<any> {
+    return await this.http.get('https://statsapi.web.nhl.com/api/v1/schedule');
+  }
+
+  // YYYY-MM-DD format for startDate, endDate
+  public async getScheduleRange(startDate: string, endDate: string, teamID: number): Promise<any> {
+    return await this.http.get('https://statsapi.web.nhl.com/api/v1/schedule?TeamId=' + teamID + '&startDate='
+    + startDate + '&endDate=' + endDate);
   }
 }
