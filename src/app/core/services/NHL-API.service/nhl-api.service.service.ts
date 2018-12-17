256fbfa2e9300cdf81714a5ec26f1af950ef194e
Hai Lin Zhang <hzhan339@hotmail.com>
2018-12-16 20:53:51 -0800

    Added additional API methods

--- src/app/core/services/NHL-API.service/nhl-api.service.service.ts
+++ src/app/core/services/NHL-API.service/nhl-api.service.service.ts
@@ -13,9 +13,7 @@ export class NHLService {
       return await this.http.get('https://statsapi.web.nhl.com/api/v1/standings');
   }
 
-    /**
-     * Gets schedule for current day
-     */
+  // Gets schedule for current day
   public async getTodaysSchedule(): Promise<any> {
     return await this.http.get('https://statsapi.web.nhl.com/api/v1/schedule');
   }
@@ -24,5 +22,17 @@ export class NHLService {
   public async getScheduleRange(startDate: string, endDate: string, teamID: number): Promise<any> {
     return await this.http.get('https://statsapi.web.nhl.com/api/v1/schedule?TeamId=' + teamID + '&startDate='
     + startDate + '&endDate=' + endDate);
+  }
+
+  public async getNextWeekSchedule(): Promise<any> {
+    const currentDate = new Date();
+    const dateNextWeek = new Date(currentDate.getTime() + 7 * 24 * 60 * 60 * 1000);
+    return await this.http.get('https://statsapi.web.nhl.com/api/v1/schedule?startDate=' + currentDate.getFullYear()
+    + '-' + currentDate.getMonth() + '-' + currentDate.getDate() + '?endDate=' + dateNextWeek.getFullYear()
+        + '-' + dateNextWeek.getMonth() + '-' + dateNextWeek.getDate());
+  }
+
+  public async getTeamRoster(ID: string): Promise<any> {
+    return await this.http.get('GET https://statsapi.web.nhl.com/api/v1/teams/' + ID + '?expand=team.roster');
   }
 }
