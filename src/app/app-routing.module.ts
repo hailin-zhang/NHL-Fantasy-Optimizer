af34e993816ad7a6f6cfb8b53280adeb973b4bf9
Hai Lin Zhang <hzhan339@hotmail.com>
2019-01-05 23:30:17 -0800

    use Angular 6 routing instead of modalController - TODO

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './modules/home/home.module#HomePageModule' },
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', loadChildren: './modules/home/home.module#HomePageModule'},
+    // {path: 'teams', loadChildren: './modules/home/home.module#DivisionTeamsComponent'},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/modules/home/page/home.page.ts
+++ src/app/modules/home/page/home.page.ts
@@ -2,7 +2,6 @@ import {Component} from '@angular/core';
 import {YahooService} from '../../../core/services/Yahoo-API.service/yahoo-api.service.service';
 import {NHLService} from '../../../core/services/NHL-API.service/nhl-api.service.service';
 import {Conference, Division, League, Standings} from '../../../shared/view-models/standings.view-model';
-import {ModalController} from '@ionic/angular';
 import {DivisionTeamsComponent} from '../components/division-teams/division-teams.component';
 
 @Component({
@@ -14,22 +13,12 @@ import {DivisionTeamsComponent} from '../components/division-teams/division-team
 export class HomePage {
 
     public currentStandings: Array<League> = [];
-    public nextWeekSchedule: Array<any> = [];
 
     constructor(private YahooAPI: YahooService,
-                private NHLApi: NHLService,
-                private modalController: ModalController) {
+                private NHLApi: NHLService,) {
         this.NHLApi.getCurrentStandings().then((standings: any) => {
             this.currentStandings = standings.records;
         });
     }
 
-    public openDivisionsModal(division: Division) {
-        this.modalController.create({
-            component: DivisionTeamsComponent,
-            componentProps: {
-                'currentDivision': division
-            }
-        });
-    }
 }
